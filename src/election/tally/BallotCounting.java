18d5f94fd32ee9cf6c93da2f3fae9492cc3e08db
Dermot Cochran <dcochran@dummy.example.com>
2009-06-12 15:36:20 +0000

    1. Refactored and renamed cluster charts. 2. Removed duplicate methods associated with handling of fractional ballots.  The Seanad/Gregory method is to duplicate each ballot, say ten thousand times, before counting.

--- src/election/tally/BallotCounting.java
+++ src/election/tally/BallotCounting.java
@@ -227,7 +227,156 @@ public class BallotCounting extends AbstractBallotCounting {
 		
 	}
 
-    // Model of the ballot counting process
+    /**
+	 * Inner class for state machine
+	 */
+	public class FractionalBallotCountingMachine implements BallotCountingModel {
+		
+		// Initial state
+		public FractionalBallotCountingMachine() {
+			state = READY_TO_COUNT;
+		}
+	
+		//@ public invariant isPossibleState (state);
+		//@ public constraint isTransition(\old(state), state);
+		private /*@ spec_public @*/ int state;
+		
+		//@ also ensures \result == state;
+		public int getState() {
+			return state;
+		}
+	
+		/**
+		 * 
+		 */
+		//@ also ensures newState == state;
+		public void changeState(int newState) {
+			state = newState;
+		}
+	
+		public boolean isPossibleState(final int value) {
+			return ((READY_TO_COUNT == value) ||
+					(NO_SEATS_FILLED_YET == value) ||
+					(CANDIDATES_HAVE_QUOTA == value) ||
+					(CANDIDATE_ELECTED == value) ||
+					(NO_SURPLUS_AVAILABLE == value) ||
+					(SURPLUS_AVAILABLE == value) ||
+					(READY_TO_ALLOCATE_SURPLUS == value) ||
+					(READY_TO_MOVE_BALLOTS == value) ||
+					(CANDIDATE_EXCLUDED == value) ||
+					(READY_FOR_NEXT_ROUND_OF_COUNTING == value) ||
+					(LAST_SEAT_BEING_FILLED == value) ||
+					(MORE_CONTINUING_CANDIDATES_THAN_REMAINING_SEATS == value) ||
+					(ONE_OR_MORE_SEATS_REMAINING == value) ||
+					(ALL_SEATS_FILLED == value) ||
+					(END_OF_COUNT == value) ||
+					(ONE_CONTINUING_CANDIDATE_PER_REMAINING_SEAT == value) ||
+					(READY_TO_REWEIGHT_BALLOTS == value));
+		}
+	
+		public boolean isTransition(final int fromState, final int toState) {
+			// Self transitions are allowed
+			if (toState == fromState) {
+				return true;
+			}
+			
+			// No transitions into the initial state
+			else if (READY_TO_COUNT == toState) {
+				return false;
+			}
+			
+			// No transitions away from final state
+			else if (END_OF_COUNT == fromState) {
+				return false;
+			}
+			
+			// Transition: Calculate Quota
+			else if ((READY_TO_COUNT == fromState) && 
+					(NO_SEATS_FILLED_YET == toState)) {
+				return true;
+			}
+			
+			// Transition: Find Highest Continuing Candidate with Quota
+			else if (((NO_SEATS_FILLED_YET == fromState) || 
+					(CANDIDATES_HAVE_QUOTA == fromState) ||
+					(MORE_CONTINUING_CANDIDATES_THAN_REMAINING_SEATS == fromState)) &&
+				((CANDIDATE_ELECTED == toState) ||	
+					(NO_SURPLUS_AVAILABLE == toState))) {
+					return true;
+				}
+			
+			// Transition: Calculate Surplus
+			else if ((CANDIDATE_ELECTED == fromState) &&
+			   ((CANDIDATES_HAVE_QUOTA == toState) ||
+					   (SURPLUS_AVAILABLE == toState) ||
+					   (NO_SURPLUS_AVAILABLE == toState))) {
+				   return true;
+			   }
+			
+			// Transition: Calculate Weight Factor
+			else if ((SURPLUS_AVAILABLE == fromState) && 
+					(READY_TO_REWEIGHT_BALLOTS == toState)) {
+				return true;
+			}
+			
+			// Transition: Weight and Transfer Ballots
+			else if ((READY_TO_REWEIGHT_BALLOTS == fromState) &&
+					(READY_FOR_NEXT_ROUND_OF_COUNTING == toState)) {
+				return true;
+			}
+			
+			// Transition: Move the Ballots
+			else if ((READY_TO_MOVE_BALLOTS == fromState) && 
+					(READY_FOR_NEXT_ROUND_OF_COUNTING == toState)) {
+				return true;
+			}
+			
+			// Transition: Calculate Transfers
+			else if ((CANDIDATE_EXCLUDED == fromState) &&
+					(READY_TO_MOVE_BALLOTS == toState)) {
+				return true;
+			}
+			
+			// Transition: Select Lowest Continuing Candidates for Exclusion
+			else if (((NO_SURPLUS_AVAILABLE == fromState) ||
+					(LAST_SEAT_BEING_FILLED == fromState)) &&
+					(CANDIDATE_EXCLUDED == toState)) {
+				return true;
+			}
+			
+			// Transition: Count Continuing Candidates
+			else if ((ONE_OR_MORE_SEATS_REMAINING == fromState) &&
+					((LAST_SEAT_BEING_FILLED == toState) ||
+					(MORE_CONTINUING_CANDIDATES_THAN_REMAINING_SEATS == toState) ||
+					(ONE_CONTINUING_CANDIDATE_PER_REMAINING_SEAT == toState))) {
+				return true;
+			}
+			
+			// Transition: Check Remaining Seats
+			else if ((READY_FOR_NEXT_ROUND_OF_COUNTING == fromState) &&
+					((ONE_OR_MORE_SEATS_REMAINING == toState) ||
+					(ALL_SEATS_FILLED == toState))) {
+				return true;
+			}
+			
+			// Transition: Declare Remaining Candidates Elected
+			else if ((ONE_CONTINUING_CANDIDATE_PER_REMAINING_SEAT == fromState) &&
+					(ALL_SEATS_FILLED == toState)) {
+				return true;
+			}
+			
+			// Transition: Close the Count
+			else if ((ALL_SEATS_FILLED == fromState) &&
+					(END_OF_COUNT == toState)) {
+				return true;
+			}
+			
+			// No other state transitions are possible
+			return false;
+		}
+	}
+
+	// Model of the ballot counting process
 	private /*@ spec_public @*/ BallotCountingModel ballotCountingMachine;
 	
 	/**
