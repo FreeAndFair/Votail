02f35e494a8a9e223954f5591df156025ff7a469
Dermot Cochran <dcochran@dummy.example.com>
2009-06-12 17:40:29 +0000

    Complete specification for the abstract state machine.

--- src/election/tally/BallotCountingModel.java
+++ src/election/tally/BallotCountingModel.java
@@ -23,38 +23,98 @@ public interface BallotCountingModel {
 	public static final int ONE_CONTINUING_CANDIDATE_PER_REMAINING_SEAT = 18;
 	public static final int READY_TO_REWEIGHT_BALLOTS = 19;
 
-	//@ public constraint \old(getState()) == READY_TO_COUNT ==> getState() == NO_SEATS_FILLED_YET; 
-	/*@ public constraint \old(getState()) == NO_SEATS_FILLED_YET ==> getState() == CANDIDATE_ELECTED || 
-	  @                                                               getState() == NO_SURPLUS_AVAILABLE;
-	  @ public constraint \old(getState()) == CANDIDATE_ELECTED ==> getState() == SURPLUS_AVAILABLE ||
-	  @                                                             getState() == NO_SURPLUS_AVAILABLE ||
-	  @                                                             getState() == CANDIDATES_HAVE_QUOTA;
-	  @ public constraint \old(getState()) == SURPLUS_AVAILABLE ==> getState() == READY_TO_ALLOCATE_SURPLUS;
-	  @ public constraint \old(getState()) == READY_TO_ALLOCATE_SURPLUS ==> getState() == READY_TO_CALCULATE_ROUNDING_TRANFSFERS;
-	  @ public constraint \old(getState()) == READY_TO_CALCULATE_ROUNDING_TRANSFERS ==> getState() == READY_TO_ADJUST_NUMBER_OF_TRANSFERS;
-	  @ public constraint \old(getState()) == READY_TO_ADJUST_NUMBER_OF_TRANSFERS ==> getState() == READY_TO_MOVE_BALLOTS;
-	  @ public constraint \old(getState()) == READY_TO_MOVE_BALLOTS ==> getState() == READY_FOR_NEXT_ROUND_OF_COUNTING;
-	  @ public constraint \old(getState()) == CANDIDATE_EXCLUDED ==> getState() == READY_TO_MOVE_BALLOTS;
-	  @ public constraint \old(getState()) == NO_SURPLUS_AVAILABLE ==> getState() == CANDIDATE_EXCLUDED;
-	  
+	/*@ public constraint \old(getState()) == READY_TO_COUNT ==> 
+	  @                   getState() == NO_SEATS_FILLED_YET;
+	  @
+	  @ public constraint \old(getState()) == NO_SEATS_FILLED_YET ==> 
+	  @                   getState() == CANDIDATE_ELECTED || 
+	  @                   getState() == NO_SURPLUS_AVAILABLE;
+	  @
+	  @ public constraint \old(getState()) == CANDIDATE_ELECTED ==> 
+	  @                   getState() == SURPLUS_AVAILABLE ||
+	  @                   getState() == NO_SURPLUS_AVAILABLE ||
+	  @                   getState() == CANDIDATES_HAVE_QUOTA;
+	  @
+	  @ public constraint \old(getState()) == SURPLUS_AVAILABLE ==> 
+	  @                   getState() == READY_TO_ALLOCATE_SURPLUS ||
+	  @                   getState() == READY_TO_REWEIGHT_BALLOTS;
+	  @
+	  @ public constraint \old(getState()) == READY_TO_ALLOCATE_SURPLUS ==> 
+	  @                   getState() == READY_TO_CALCULATE_ROUNDING_TRANSFERS;
+	  @
+	  @ public constraint \old(getState()) == READY_TO_CALCULATE_ROUNDING_TRANSFERS ==> 
+	  @                   getState() == READY_TO_ADJUST_NUMBER_OF_TRANSFERS;
+	  @
+	  @ public constraint \old(getState()) == READY_TO_ADJUST_NUMBER_OF_TRANSFERS ==> 
+	  @                   getState() == READY_TO_MOVE_BALLOTS;
+	  @
+	  @ public constraint \old(getState()) == READY_TO_MOVE_BALLOTS ==> 
+	  @                   getState() == READY_FOR_NEXT_ROUND_OF_COUNTING;
+	  @
+	  @ public constraint \old(getState()) == CANDIDATE_EXCLUDED ==> 
+	  @                   getState() == READY_TO_MOVE_BALLOTS;
+	  @
+	  @ public constraint \old(getState()) == NO_SURPLUS_AVAILABLE ==> 
+	  @                   getState() == CANDIDATE_EXCLUDED;
+	  @
+	  @ public constraint \old(getState()) == READY_FOR_NEXT_ROUND_OF_COUNTING ==> 
+	  @                   getState() == ALL_SEATS_FILLED ||
+	  @                   getState() == ONE_OR_MORE_SEATS_REMAINING;
+	  @
+	  @ public constraint \old(getState()) == ALL_SEATS_FILLED ==> 
+	  @                   getState() == END_OF_COUNT;
+	  @
+	  @ public constraint \old(getState()) == ONE_OR_MORE_SEATS_REMAINING ==> 
+	  @                   getState() == MORE_CONTINUING_CANDIDATES_THAN_REMAINING_SEATS ||
+	  @                   getState() == ONE_CONTINUING_CANDIDATE_PER_REMAINING_SEAT ||
+	  @                   getState() == LAST_SEAT_BEING_FILLED ||
+	  @                   getState() == ONE_CONTINUING_CANDIDATE_PER_REMAINING_SEAT;
+	  @
+	  @ public constraint \old(getState()) == MORE_CONTINUING_CANDIDATES_THAN_REMAINING_SEATS ==>
+	  @                   getState() == CANDIDATE_ELECTED ||
+	  @                   getState() == NO_SURPLUS_AVAILABLE;
+	  @
+	  @ public constraint \old(getState()) == LAST_SEAT_BEING_FILLED ==>
+	  @                   getState() == CANDIDATE_EXCLUDED;
+	  @
+	  @ public constraint \old(getState()) == READY_TO_REWEIGHT_BALLOTS ==>
+	  @                   getState() == READY_FOR_NEXT_ROUND_OF_COUNTING;
+	  @
+	  @ public constraint \old(getState()) == ONE_CONTINUING_CANDIDATE_PER_REMAINING_SEAT ==>
+	  @                   getState() == ALL_SEATS_FILLED;
+	  @
+	  @ public constraint (* No other state transitions are possible *);
 	  @*/
-	//@ ensures isPossibleState(\result);
-	//@ public initially getState() == READY_TO_COUNT;
+	  
+	/**
+	 * Get the current stage of counting.
+	 */
+	 //@ ensures isPossibleState(\result);
 	public abstract /*@ pure @*/ int getState();
 
+	/**
+	 * Move to the next stage of counting.
+	 * 
+	 * @param newState The next stage of counting.
+	 */
 	//@ requires isPossibleState (newState);
 	//@ requires isTransition (getState(), newState);
 	//@ ensures getState() == newState;
 	public abstract void changeState(int newState);
 
 	/**
-	 * Set of possible states
+	 * Confirm that this value is a valid stage of counting.
+	 * 
+	 * @param value The stage in the counting process.
 	 */
 	//@ ensures \result <==> (READY_TO_COUNT <= value && value <= READY_TO_REWEIGHT_BALLOTS);
 	public abstract /*@ pure @*/ boolean isPossibleState(int value);
 
 	/**
-	 * Set of valid transitions from state machine diagram
+	 * Confirm that this is a valid transition from one stage of counting to another.
+	 * 
+	 * @param fromState The current stage of counting.
+	 * @param toState The next stage if counting.
 	 */
 	//@ requires isPossibleState(fromState);
 	//@ requires isPossibleState(toState);
