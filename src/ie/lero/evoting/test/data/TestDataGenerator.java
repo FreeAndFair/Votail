504ab9401ef0d92b52175dc13d66c9efcf43829b
Dermot Cochran <dcochran@dummy.example.com>
2009-11-09 11:38:15 +0000

    Move JML unit test generated files to source path

--- src/ie/lero/evoting/test/data/TestDataGenerator.java
+++ src/ie/lero/evoting/test/data/TestDataGenerator.java
@@ -3,7 +3,7 @@ package ie.lero.evoting.test.data;
 import election.tally.Ballot;
 import election.tally.Candidate;
 
-public class Generator {
+public class TestDataGenerator {
   
   Ballot generateBallot(int number) {
     int [] preferences = new int [number];
