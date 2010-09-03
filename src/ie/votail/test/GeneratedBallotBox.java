fbdef2c562057abf73848bc8df7ac608967e185a
Dermot Cochran <dcochran@dummy.example.com>
2010-09-03 14:41:51 +0000

    Create data structure for import and export of ballot box test data

---
+++ src/ie/votail/test/GeneratedBallotBox.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package ie.votail.test;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import election.tally.BallotBox;
+
+/**
+ * @author dero
+ * @version 0.0.2
+ */
+public class GeneratedBallotBox extends BallotBox {
+
+  /**
+   * Ballot box that be written to external storage or read from external storage
+   */
+  public GeneratedBallotBox() {
+    // TODO Auto-generated constructor stub
+  }
+
+  private void write(java.io.ObjectOutputStream out) throws IOException {
+  }
+
+  private void read(java.io.ObjectInputStream in) throws IOException,
+      ClassNotFoundException {
+  }
+
+}
