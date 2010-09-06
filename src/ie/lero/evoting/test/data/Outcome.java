a23428102951fd337430ad6fdc62d4c81e132aa2
Dermot Cochran <dcochran@dummy.example.com>
2010-09-06 13:45:00 +0000

    Complete algorithm for generate of scenarios with valid outcomes from a sequence of seed integers

--- src/ie/lero/evoting/test/data/Outcome.java
+++ src/ie/lero/evoting/test/data/Outcome.java
@@ -2,20 +2,51 @@ package ie.lero.evoting.test.data;
 
 public class Outcome {
   
-  public static int SORELOSER = 0;
-  
-  
-  
-  
-  
+  // Distinct outcomes for each Candidate
+  public static int SORE_LOSER = 0;
+  public static int TIED_SORE_LOSER = 1;
+  public static int EARLY_LOSER = 2;
+  public static int TIED_EARLY_LOSER = 3;
+  public static int LOSER = 4;
+  public static int TIED_LOSER = 5;
+  public static int TIED_WINNER = 6;
+  public static int COMPROMISE_WINNER = 7;
+  public static int QUOTA_WINNER = 8;
   public static int WINNER = 9;
   
-  public static int Radix = 10;
+  // Number of distinct outcomes
+  public static int Radix = WINNER + 1;
+
   
   private int value;
 
-  
+  // Convert any integer into an outcome
   public Outcome (int outcome) {
-    value = outcome;
+    value = outcome % Radix;
+  }
+  
+  public Outcome() {
+    value = WINNER;
+  }
+  
+  /**
+   * Generate an outcome within a restricted range
+   * 
+   * @param base The seed value
+   * @param minimum The minimum outcome
+   * @param maximum The maximum outcome
+   */
+  /*
+   * requires minimum < maximum;
+   * requires SORE_LOSER <= minimum;
+   * requires maximum <= WINNER;
+   */
+  
+  public Outcome(int base, int minimum, int maximum) {
+    value = minimum + base % (1 + maximum - minimum);
+  }
+
+  public int getValue() {
+    return value;
   }
 }
--- src/ie/lero/evoting/test/data/Scenario.java
+++ src/ie/lero/evoting/test/data/Scenario.java
@@ -1,7 +1,6 @@
 package ie.lero.evoting.test.data;
 
 public class Scenario {
-  public static int EFFECTIVE_CANDIDATES = 5;
   Outcome[] outcomes;
   private int length;
 
@@ -10,11 +9,42 @@ public class Scenario {
     length = numberOfCandidates;
   }
   
-  public void generate (int seed) {
-    int base = 1;
-    for (int i=0;i < length; i++) {
-      outcomes[i] = new Outcome ((seed % (Outcome.Radix * base)) / base); 
-      base *= Outcome.Radix;
+  /**
+   * Use a seed number to generate a sequence of outcomes
+   * 
+   * @param seed The generator for the sequence
+   */
+  public void generate (int seed, int winners) {
+    int base = seed;
+    for (int i=0; i<winners; i++) {
+      outcomes[i] = winner(base);
+      base /= Outcome.Radix;
     }
+    for (int i=winners; i<length; i++) {
+      outcomes[i] = loser(base); 
+      base /= Outcome.Radix;
+    }
+  }
+
+  private Outcome loser(int base) {
+    return new Outcome (base, Outcome.SORE_LOSER, Outcome.TIED_LOSER);
+  }
+
+  private Outcome winner(int base) {
+    return new Outcome(base, Outcome.TIED_WINNER, Outcome.WINNER);
+  }
+  
+  public int getLength() {
+    return length;
+  }
+  
+  /** Get the outcome for any integer index, wrapping around if needed
+   * 
+   * @param n The index
+   * @return The candidate outcome
+   */
+  public Outcome getOutcome (int n) {
+    int index = n % length;
+    return outcomes[index];
   }
 }
