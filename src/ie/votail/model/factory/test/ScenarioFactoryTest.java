3f1b3c102626f770b3879cd77149b571cf1feb04
Dermot Cochran <dcochran@dummy.example.com>
2010-10-25 15:25:17 +0000

    Added unit tests for two and three candidate scenarios

--- src/ie/votail/model/factory/test/ScenarioFactoryTest.java
+++ src/ie/votail/model/factory/test/ScenarioFactoryTest.java
@@ -1,14 +1,23 @@
 package ie.votail.model.factory.test;
 
 import static org.junit.Assert.*;
+import ie.votail.model.factory.ScenarioFactory;
+import ie.votail.model.factory.ScenarioList;
 
 import org.junit.Test;
 
 public class ScenarioFactoryTest {
 
   @Test
-  public void testFind() {
-    fail("Not yet implemented");
+  public void testTwoCandidateScenarios() {
+    ScenarioList twoCandidateScenarios = ScenarioFactory.find(2);
+    assertEquals (twoCandidateScenarios.size(),4);
+  }
+  
+  @Test
+  public void testThreeCandidateScenarios() {
+    ScenarioList threeCandidateScenarios = ScenarioFactory.find(3);
+    assertEquals (threeCandidateScenarios.size(),26);
   }
 
 }
