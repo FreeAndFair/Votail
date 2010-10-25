3fd5f10ce0e227b921a0c4145db4e49e96c9ae8a
Dermot Cochran <dcochran@dummy.example.com>
2010-10-25 15:10:50 +0000

    Adding empty test case

---
+++ src/ie/votail/model/factory/test/ScenarioFactoryTest.java
@@ -0,0 +1,14 @@
+package ie.votail.model.factory.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ScenarioFactoryTest {
+
+  @Test
+  public void testFind() {
+    fail("Not yet implemented");
+  }
+
+}
