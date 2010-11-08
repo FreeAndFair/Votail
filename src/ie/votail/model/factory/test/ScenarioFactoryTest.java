2eb5a47aed02326d1a0825cd870eebe317717199
Dermot Cochran <dcochran@dummy.example.com>
2010-11-08 14:24:48 +0000

    Added simple unit tests for scenario generation

--- src/ie/votail/model/factory/test/ScenarioFactoryTest.java
+++ src/ie/votail/model/factory/test/ScenarioFactoryTest.java
@@ -1,23 +1,38 @@
 package ie.votail.model.factory.test;
 
-import static org.junit.Assert.*;
 import ie.votail.model.factory.ScenarioFactory;
 import ie.votail.model.factory.ScenarioList;
+import junit.framework.TestCase;
 
 import org.junit.Test;
 
-public class ScenarioFactoryTest {
+public class ScenarioFactoryTest extends TestCase {
 
   @Test
   public void testTwoCandidateScenarios() {
-    ScenarioList twoCandidateScenarios = ScenarioFactory.find(2);
-    assertEquals (twoCandidateScenarios.size(),4);
+    ScenarioFactory scenarioFactory = new ScenarioFactory();
+    ScenarioList twoCandidateScenarios = scenarioFactory.find(2);
+    assertEquals (4,twoCandidateScenarios.size());
   }
   
   @Test
   public void testThreeCandidateScenarios() {
-    ScenarioList threeCandidateScenarios = ScenarioFactory.find(3);
-    assertEquals (threeCandidateScenarios.size(),26);
+    ScenarioFactory scenarioFactory = new ScenarioFactory();
+    ScenarioList threeCandidateScenarios = scenarioFactory.find(3);
+    assertEquals (29,threeCandidateScenarios.size());
   }
 
+  @Test
+  public void testFourCandidateScenarios() {
+    ScenarioFactory scenarioFactory = new ScenarioFactory();
+    ScenarioList threeCandidateScenarios = scenarioFactory.find(4);
+    assertEquals (217,threeCandidateScenarios.size());
+  }
+  
+  @Test
+  public void testFiveCandidateScenarios() {
+    ScenarioFactory scenarioFactory = new ScenarioFactory();
+    ScenarioList threeCandidateScenarios = scenarioFactory.find(5);
+    assertEquals (1673,threeCandidateScenarios.size());
+  }
 }
