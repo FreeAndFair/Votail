c353e54f2f111d8a3e8fc6852094a4cfa9e907d0
Dermot Cochran <dcochran@dummy.example.com>
2010-11-15 14:08:57 +0000

    New unit tests fro Ballot.toString and BallotBox.toString

---
+++ src/election/tally/test/BallotBoxTest.java
@@ -0,0 +1,18 @@
+package election.tally.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import election.tally.BallotBox;
+
+public class BallotBoxTest {
+  @Test
+  public void testBallotBox_toString() {
+    int [] preferences = {1,2,3};
+    BallotBox box = new BallotBox();
+    box.accept(preferences);
+    Assert.assertEquals(box.toString(),"(1 2 3)");
+    box.accept(preferences);
+    Assert.assertEquals(box.toString(),"(1 2 3) (1 2 3)");
+  }
+}
---
+++ src/election/tally/test/BallotTest.java
@@ -0,0 +1,17 @@
+package election.tally.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import election.tally.Ballot;
+
+public class BallotTest {
+  @Test
+  public void testBallot_toString() {
+    int [] preferences = {1,2,3};
+    Ballot ballot = new Ballot(preferences);
+    Assert.assertEquals(ballot.toString(),"1 2 3");
+    
+  }
+
+}
