a041657836ee4e0348025abe36c11c8008f9c0e8
Dermot Cochran <dcochran@dummy.example.com>
2010-12-13 15:37:38 +0000

    Outline of algorithm for coverting VoteTable into BallotBox

--- src/ie/votail/model/VoteTable.java
+++ src/ie/votail/model/VoteTable.java
@@ -11,7 +11,7 @@ import election.tally.BallotBox;
 
 public class VoteTable {
   
-  protected int numberOfBallots;
+  protected int numberOfBallots = 0;
   protected ArrayList<Vote> votes;
 
   /**
@@ -20,13 +20,16 @@ public class VoteTable {
    */
   public VoteTable(A4Solution solution) {
     
+    votes = new ArrayList<Vote>();
+    
     // Iterate through the solution and add each vote to the table
     for (Sig sig : solution.getAllReachableSigs()) {
       if (sig.label.contains("Vote")) {
         A4TupleSet tupleSet = solution.eval(sig);
         for (A4Tuple tuple : tupleSet) {
           // Tuple should consist of ballotID, candidateID and ranking
-          this.add(new Vote(tuple));
+          votes.add(new Vote(tuple));
+          numberOfBallots++;
         }
       }
     }
@@ -37,12 +40,50 @@ public class VoteTable {
    * @param vote
    */
   public void add(/*@ non_null*/ Vote vote) {
-    this.add(vote);
+    votes.add(vote);
+    numberOfBallots++;
   }
 
+  /**
+   * 
+   * @return
+   */
   public BallotBox getBallotBox() {
+    BallotBox box = new BallotBox();
+    
+    int[] ballotIDs = this.getBallotIDs();
+    
+    for (int i=0; i < numberOfBallots; i++) {
+      int ballotID = ballotIDs[i];
+      int numberOfRankings = this.getNumberOfRankings(ballotID);
+      int[] preferences = new int[numberOfRankings];
+      
+      for (int ranking=1; ranking <= numberOfRankings; ranking++) {
+        preferences[ranking] = this.getCandidateID(ballotID,ranking);
+      }
+      box.accept(preferences);
+    }
+    return box;
+  }
+
+  private int getCandidateID(int ballotID, int ranking) {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  private int getNumberOfRankings(int ballotID) {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  private int[] getBallotIDs() {
     // TODO Auto-generated method stub
     return null;
+  }
+
+  private int getNumberOfBallots() {
+    // TODO Auto-generated method stub
+    return 0;
   }
 
 }
