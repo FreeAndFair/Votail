af51c2adc4a7843eaab7e4df0bb5785c6ec4bf99
Dermot Cochran <dcochran@dummy.example.com>
2011-01-10 15:49:13 +0000

    Generalise into a full system test

--- src/ie/votail/model/factory/test/VotailSystemTest.java
+++ src/ie/votail/model/factory/test/VotailSystemTest.java
@@ -1,5 +1,8 @@
+// 2010-11, Dermot Cochran, IT University of Copenhagen
+
 package ie.votail.model.factory.test;
 
+import org.testng.annotations.Test;
 import ie.votail.model.ElectionConfiguration;
 import ie.votail.model.ElectoralScenario;
 import ie.votail.model.factory.BallotBoxFactory;
@@ -8,36 +11,41 @@ import ie.votail.model.factory.ScenarioList;
 
 import java.util.logging.Logger;
 
-import org.junit.Test;
-
 import election.tally.BallotCounting;
 import election.tally.Constituency;
 
 public class VotailSystemTest {
-  @Test
-  public void twoCandidateTest() {
-    
-    ScenarioFactory scenarioFactory = new ScenarioFactory();
-    BallotCounting ballotCounting = new BallotCounting();
-    Logger logger = Logger.getLogger(BallotBoxFactory.LOG_FILENAME);
-    
-    final int candidates = 20;
-    final int seats = 3;
-    ScenarioList scenarios = scenarioFactory.find(candidates,seats);
-    
-    for (ElectoralScenario scenario: scenarios) {
-      logger.info(scenario.toString());
-      final int scope = 5;
-      BallotBoxFactory ballotBoxFactory = new BallotBoxFactory();
-      ElectionConfiguration electionConfiguration 
-        = ballotBoxFactory.extractBallots(scenario, scope);
-      Constituency constituency = electionConfiguration.getConstituency();
-      logger.info(constituency.toString());
-      logger.info(electionConfiguration.toString());
-      ballotCounting.setup(constituency);
-      ballotCounting.load(electionConfiguration);
-      ballotCounting.count();
-      logger.info(ballotCounting.getResults());
-    }
-  }
+	@Test
+	public void fullSystemsTest() {
+
+		final int scope = 5;
+
+		ScenarioFactory scenarioFactory = new ScenarioFactory();
+		BallotCounting ballotCounting = new BallotCounting();
+		Logger logger = Logger.getLogger(BallotBoxFactory.LOG_FILENAME);
+
+		for (int candidates = 6; candidates <= 50; candidates++) {
+			for (int seats = 3; seats <= 5; seats++) {
+				ScenarioList scenarioList = scenarioFactory.find(candidates,
+						seats);
+
+				for (ElectoralScenario scenario : scenarioList) {
+					logger.info(scenario.toString());
+					BallotBoxFactory ballotBoxFactory = new BallotBoxFactory();
+					ElectionConfiguration electionConfiguration = ballotBoxFactory
+							.extractBallots(scenario, scope);
+					Constituency constituency = electionConfiguration
+							.getConstituency();
+					logger.info(constituency.toString());
+					logger.info(electionConfiguration.toString());
+					ballotCounting.setup(constituency);
+					ballotCounting.load(electionConfiguration);
+					ballotCounting.count();
+					String results = ballotCounting.getResults();
+					logger.info(results);
+					// TODO compare results with scenario
+				}
+			}
+		}
+	}
 }
