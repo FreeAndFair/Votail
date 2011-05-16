3555e99ebac5d7338244e180ae5760b9515751e5
Dermot Cochran <dcochran@dummy.example.com>
2011-05-16 14:04:10 +0000

    Complete - task : rereading of test data during generation

--- src.test/ie/votail/uilioch/UniversalTestGenerator.java
+++ src.test/ie/votail/uilioch/UniversalTestGenerator.java
@@ -2,6 +2,7 @@
 
 package ie.votail.uilioch;
 
+import flexjson.JSONDeserializer;
 import flexjson.JSONSerializer;
 import ie.votail.model.ElectionConfiguration;
 import ie.votail.model.ElectoralScenario;
@@ -11,6 +12,7 @@ import ie.votail.model.factory.ScenarioFactory;
 import ie.votail.model.factory.ScenarioList;
 
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.logging.Logger;
@@ -38,15 +40,14 @@ public class UniversalTestGenerator {
    * @param method
    */
   public void generateTests(final int numberOfSeats,
-      final int numberOfCandidates, final Method method) {
+      final int numberOfCandidates, final Method method,
+      final boolean appendToFile) {
     
     try {
-      FileWriter writer = new FileWriter(getFilename(method));
+      FileWriter writer = new FileWriter(getFilename(method),appendToFile);
       
       for (int seats = 1; seats <= numberOfSeats; seats++) {
         for (int candidates = 1 + seats; candidates <= numberOfCandidates; candidates++) {
-          
-          // TODO check if ballot box already generated
           
           createBallotBoxes(seats, candidates, method, writer);
         }
@@ -81,12 +82,15 @@ public class UniversalTestGenerator {
     
     for (ElectoralScenario scenario : scenarioList) {
       logger.info(scenario.toString());
+      
+      // TODO check if ballot box already generated
+      if (notAlreadyGenerated(scenario)) {
+      
       ElectionConfiguration electionData =
           ballotBoxFactory.extractBallots(scenario, candidates);
       
       try {
         
-        // FIXME also include preferenceList
         serializer.include("ballots.preferenceList", "candidateIDs").serialize(
             electionData, writer);
       }
@@ -95,25 +99,67 @@ public class UniversalTestGenerator {
             + e.getCause());
       }
       count++;
+      }
     }
     
     logger.info("Generated " + count + " scenarios for " + method.toString()
-        + " with " + candidates + " for " + seats + "seats.");
+        + " with " + candidates + " candidates for " + seats + " seats.");
   }
   
   /**
+   * Ensure that a ballot box has not already been generated for this scenario
+   * 
+   * @param scenario
+   * @return
+   */
+ protected boolean notAlreadyGenerated(ElectoralScenario scenario) {
+   final String filename = getFilename(scenario.getMethod());
+   try {
+     
+     FileReader reader = new FileReader (filename);
+     
+     while (reader.ready()) {
+       
+       ElectionConfiguration electionConfiguration =
+         new JSONDeserializer<ElectionConfiguration>().deserialize(reader);
+       
+       if (scenario.equivalentTo(electionConfiguration.getScenario())) {
+         return false;
+       }
+     }
+     reader.close();
+   }
+   catch (IOException e) {
+     logger.severe("Failed to read scenarios from file " + filename
+         + " because " + e.getMessage());
+   }
+   finally {
+     logger.info("Finished!");
+   }
+    return true;
+  }
+
+ /**
    * Get name of the file which contains testdata for this method.
    * 
    * @param method The type of voting scheme
-   * @return
+   * @return The filename
    */
   protected /*@ pure @*/ static String getFilename(final Method method) {
     return FILENAME_PREFIX + method.toString() + FILENAME_SUFFIX;
   }
   
+  /**
+   * Generate enough test data for 100% coverage
+   * 
+   * @param args Command line arguments (not used)
+   */
   public static void main(String[] args) {
     UniversalTestGenerator uilioch = new UniversalTestGenerator();
-    uilioch.generateTests(5, 11, Method.STV);
-    uilioch.generateTests(1, 7, Method.Plurality);
+    // TODO Command Line Interface
+    if (1 <= args.length) {
+      uilioch.generateTests(5, 11, Method.STV, true);
+      uilioch.generateTests(1, 7, Method.Plurality, true);
+    }
   }
 }
