aac6bbd4c53bb1345ed78258dcbd80b968d3583d
Dermot Cochran <dcochran@dummy.example.com>
2011-06-28 13:11:27 +0000

    Incomplete - task : pre-conditions should not be used as invariants; preconditions apply to parameters and invariants to fields, except for state-machine fields

--- src/election/tally/CandidateStatus.java
+++ src/election/tally/CandidateStatus.java
@@ -1,5 +1,11 @@
 package election.tally;
 
+/**
+ * Abstract State Machine for <code>election.tally.Candidate</code> class.
+ * 
+ * @author Dermot Cochran
+ */
+
 public class CandidateStatus {
   
   /** State value for a candidate neither elected nor eliminated yet */
@@ -15,4 +21,13 @@ public class CandidateStatus {
    * of the lowest continuing candidates at the end of a round of counting.
    */
   public static final byte ELIMINATED = 2;
+  
+  /** Abstract State Machine (ASM) specification */
+  /*@ public invariant state == ELECTED || state == ELIMINATED ||
+    @   state == CONTINUING;
+    @ public initially state == CONTINUING;
+    @ public constraint \old(state) == ELECTED ==> state == ELECTED;
+    @ public constraint \old(state) == ELIMINATED ==> state == ELIMINATED;
+    @*/
+  protected /*@ spec_public @*/ byte state = CONTINUING;
 }
