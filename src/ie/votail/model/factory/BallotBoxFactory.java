c350e429eb9c76eb6aa9997cc622309adea115d5
Dermot Cochran <dcochran@dummy.example.com>
2011-07-15 13:26:39 +0000

    Incomplete - task : Latest generation of test data

--- src/ie/votail/model/factory/BallotBoxFactory.java
+++ src/ie/votail/model/factory/BallotBoxFactory.java
@@ -41,7 +41,7 @@ public class BallotBoxFactory {
   public static final String LOGGER_NAME = "votail.log";
   public static final String MODELS_VOTING_ALS = "models/Voting.als";
   protected final static Logger logger = Logger.getLogger(LOGGER_NAME);
-  protected static final int MAX_SCOPE = 15;
+  protected static final int MAX_SCOPE = 45;
   protected String modelName;
   
   /**
